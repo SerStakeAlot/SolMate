7ebed72a28cfa3eac58b5962783f06d9e799d963
SerStakeAlot <ognicoscotti@gmail.com>
2026-01-14 17:31:51 +0000

    Add Digital Asset Links for TWA full-screen mode

---
+++ android-twa/app/src/main/java/app/netlify/shiny_chebakia_11939c/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package app.netlify.shiny_chebakia_11939c.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
