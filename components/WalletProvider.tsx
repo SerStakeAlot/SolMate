bae691552c93af388cb68fdbfe0dd723c6f0b56a
SerStakeAlot <ognicoscotti@gmail.com>
2026-01-15 00:03:25 +0000

    🚀 MAINNET LAUNCH - Switch to mainnet-beta network

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -12,7 +12,7 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 
 export const WalletProvider: FC<{ children: ReactNode }> = ({ children }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
 
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
